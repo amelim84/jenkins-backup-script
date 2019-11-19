e25aa3f1d264cb497559762beb72ecc9a778b05b
Andre Melim <amelim@ACM000202.local>
2019-11-19 13:48:38 -0500

    WIP

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -41,7 +41,7 @@ function backup_jobs() {
       [ "${job_name}" = ".." ] && continue
       [ -d "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}" ] && mkdir -p "${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
       DST_PATH="${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
-      for TESTFILE in $(find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {})
+      for TESTFILE in $(find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0)
       do
           echo 'wooah'
           echo $TESTFILE
