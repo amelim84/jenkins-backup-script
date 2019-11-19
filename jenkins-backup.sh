7694e25ddba8e05e45158df8c41f41505029f259
Andre Melim <amelim@ACM000202.local>
2019-11-19 13:29:29 -0500

    WIP

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -38,7 +38,7 @@ function backup_jobs() {
       [ "${job_name}" = ".." ] && continue
       [ -d "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}" ] && mkdir -p "${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
       DST_PATH="${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
-      find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {} echo {}
+      find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 | PARAMS=`xargs -0 -I {} echo {}`
       getname $PARAMS $DST_PATH
       find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0
       if [ -f "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/config.xml" ] && [ "$(grep -c "com.cloudbees.hudson.plugins.folder.Folder" "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/config.xml")" -ge 1 ] ; then
