3d43a242ef0b020bc83a78ee980b747092ae5f9b
Andre Melim <amelim@ACM000202.local>
2019-11-19 11:54:19 -0500

    WIP

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -32,7 +32,7 @@ function backup_jobs() {
       [ "${job_name}" = ".." ] && continue
       [ -d "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}" ] && mkdir -p "${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
       # find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {} echo {} "${ARC_DIR}/jobs/${rel_depth}/${job_name}/"$(basename -- "{}") "END"
-      find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 -exec echo $(basename -- {}) +
+      find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 -exec echo $(basename -- "{}") +
       if [ -f "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/config.xml" ] && [ "$(grep -c "com.cloudbees.hudson.plugins.folder.Folder" "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/config.xml")" -ge 1 ] ; then
         #echo "Folder! $JENKINS_HOME/jobs/$rel_depth/$job_name/jobs"
         backup_jobs "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/jobs"
