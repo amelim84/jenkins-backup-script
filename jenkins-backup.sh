55711b63bd77f00334ebc38401c19af53ab4bbbc
Andre Melim <amelim@ACM000202.local>
2019-11-19 14:15:38 -0500

    WIP

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -20,15 +20,6 @@ function usage() {
 }
 
 
-function getname() {
-    for var in "$@"
-    do
-        echo $var
-    done
-}
-export -f getname
-
-
 function backup_jobs() {
   local run_in_path="$1"
   local rel_depth=${run_in_path#${JENKINS_HOME}/jobs/}
@@ -40,13 +31,7 @@ function backup_jobs() {
       [ "${job_name}" = "." ] && continue
       [ "${job_name}" = ".." ] && continue
       [ -d "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}" ] && mkdir -p "${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
-      TEMP=`find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml"`
-      while read -r TESTFILE;
-      do
-          echo 'wooah'
-          echo $TESTFILE
-          echo 'haoow'
-      done <<< $TEMP
+      find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {} cp -u {} "${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
       if [ -f "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/config.xml" ] && [ "$(grep -c "com.cloudbees.hudson.plugins.folder.Folder" "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/config.xml")" -ge 1 ] ; then
         #echo "Folder! $JENKINS_HOME/jobs/$rel_depth/$job_name/jobs"
         backup_jobs "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/jobs"
@@ -72,42 +57,46 @@ function main() {
     exit 1
   fi
 
-  rm -rf "${ARC_DIR}" "{$TMP_TAR_NAME}"
   for plugin in plugins jobs users secrets nodes; do
-    mkdir -p "${ARC_DIR}/${plugin}"
+    if [ ! -d "${ARC_DIR}/${plugin}" ]
+    then
+        mkdir -p "${ARC_DIR}/${plugin}"
+    fi
   done
 
-  cp "${JENKINS_HOME}/"*.xml "${ARC_DIR}"
+  cp -u "${JENKINS_HOME}/"*.xml "${ARC_DIR}"
 
-  cp "${JENKINS_HOME}/plugins/"*.[hj]pi "${ARC_DIR}/plugins"
+  cp -u "${JENKINS_HOME}/plugins/"*.[hj]pi "${ARC_DIR}/plugins"
   hpi_pinned_count=$(find ${JENKINS_HOME}/plugins/ -name *.hpi.pinned | wc -l)
   jpi_pinned_count=$(find ${JENKINS_HOME}/plugins/ -name *.jpi.pinned | wc -l)
   if [ ${hpi_pinned_count} -ne 0 -o ${jpi_pinned_count} -ne 0 ]; then
-    cp "${JENKINS_HOME}/plugins/"*.[hj]pi.pinned "${ARC_DIR}/plugins"
+    cp -u "${JENKINS_HOME}/plugins/"*.[hj]pi.pinned "${ARC_DIR}/plugins"
   fi
 
   if [ "$(ls -A ${JENKINS_HOME}/users/)" ]; then
-    cp -R "${JENKINS_HOME}/users/"* "${ARC_DIR}/users"
+    cp -uR "${JENKINS_HOME}/users/"* "${ARC_DIR}/users"
   fi
 
   if [ "$(ls -A ${JENKINS_HOME}/secrets/)" ] ; then
-    cp -R "${JENKINS_HOME}/secrets/"* "${ARC_DIR}/secrets"
+    cp -uR "${JENKINS_HOME}/secrets/"* "${ARC_DIR}/secrets"
   fi
 
   if [ "$(ls -A ${JENKINS_HOME}/nodes/)" ] ; then
-    cp -R "${JENKINS_HOME}/nodes/"* "${ARC_DIR}/nodes"
+    cp -uR "${JENKINS_HOME}/nodes/"* "${ARC_DIR}/nodes"
   fi
 
   if [ "$(ls -A ${JENKINS_HOME}/jobs/)" ] ; then
     backup_jobs ${JENKINS_HOME}/jobs/
   fi
 
-  cd "${TMP_DIR}"
-  tar -czvf "${TMP_TAR_NAME}" "${ARC_NAME}/"*
-  cd -
-  mv -f "${TMP_TAR_NAME}" "${DEST_FILE}"
+  # cd "${TMP_DIR}"
+  # tar -czvf "${TMP_TAR_NAME}" "${ARC_NAME}/"*
+  # cd -
+  # mv -f "${TMP_TAR_NAME}" "${DEST_FILE}"
+  #
+  # cleanup
 
-  cleanup
+  ls -R ${ARC_DIR}
 
   exit 0
 }
