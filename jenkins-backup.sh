08d4df7d7ff8024cf82838a33653d1274b7fa11b
Andre Melim <amelim@ACM000202.local>
2019-11-19 13:44:02 -0500

    WIP

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -41,8 +41,12 @@ function backup_jobs() {
       [ "${job_name}" = ".." ] && continue
       [ -d "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}" ] && mkdir -p "${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
       DST_PATH="${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
-      find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {} basename {}
-
+      for files in $(find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {});
+      do
+          echo 'wooah'
+          echo $files
+          echo 'haoow'
+      done
       if [ -f "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/config.xml" ] && [ "$(grep -c "com.cloudbees.hudson.plugins.folder.Folder" "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/config.xml")" -ge 1 ] ; then
         #echo "Folder! $JENKINS_HOME/jobs/$rel_depth/$job_name/jobs"
         backup_jobs "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/jobs"
