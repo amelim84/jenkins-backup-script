dc620eafe1b4ba38167c18e0f777a933fb8f0466
Andre Melim <amelim@ACM000202.local>
2019-11-19 14:27:23 -0500

    WIP

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -32,7 +32,7 @@ function backup_jobs() {
       [ "${job_name}" = "." ] && continue
       [ "${job_name}" = ".." ] && continue
       [ -d "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}" ] && mkdir -p "${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
-      find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {} cp -u {} "${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
+      find "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {} cp {} "${ARC_DIR}/jobs/${rel_depth}/${job_name}/"
       if [ -f "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/config.xml" ] && [ "$(grep -c "com.cloudbees.hudson.plugins.folder.Folder" "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/config.xml")" -ge 1 ] ; then
         #echo "Folder! $JENKINS_HOME/jobs/$rel_depth/$job_name/jobs"
         backup_jobs "${JENKINS_HOME}/jobs/${rel_depth}/${job_name}/jobs"
@@ -65,25 +65,25 @@ function main() {
     fi
   done
 
-  cp -u "${JENKINS_HOME}/"*.xml "${ARC_DIR}"
+  cp "${JENKINS_HOME}/"*.xml "${ARC_DIR}"
 
-  cp -u "${JENKINS_HOME}/plugins/"*.[hj]pi "${ARC_DIR}/plugins"
+  cp "${JENKINS_HOME}/plugins/"*.[hj]pi "${ARC_DIR}/plugins"
   hpi_pinned_count=$(find ${JENKINS_HOME}/plugins/ -name *.hpi.pinned | wc -l)
   jpi_pinned_count=$(find ${JENKINS_HOME}/plugins/ -name *.jpi.pinned | wc -l)
   if [ ${hpi_pinned_count} -ne 0 -o ${jpi_pinned_count} -ne 0 ]; then
-    cp -u "${JENKINS_HOME}/plugins/"*.[hj]pi.pinned "${ARC_DIR}/plugins"
+    cp "${JENKINS_HOME}/plugins/"*.[hj]pi.pinned "${ARC_DIR}/plugins"
   fi
 
   if [ "$(ls -A ${JENKINS_HOME}/users/)" ]; then
-    cp -uR "${JENKINS_HOME}/users/"* "${ARC_DIR}/users"
+    cp -R "${JENKINS_HOME}/users/"* "${ARC_DIR}/users"
   fi
 
   if [ "$(ls -A ${JENKINS_HOME}/secrets/)" ] ; then
-    cp -uR "${JENKINS_HOME}/secrets/"* "${ARC_DIR}/secrets"
+    cp -R "${JENKINS_HOME}/secrets/"* "${ARC_DIR}/secrets"
   fi
 
   if [ "$(ls -A ${JENKINS_HOME}/nodes/)" ] ; then
-    cp -uR "${JENKINS_HOME}/nodes/"* "${ARC_DIR}/nodes"
+    cp -R "${JENKINS_HOME}/nodes/"* "${ARC_DIR}/nodes"
   fi
 
   if [ "$(ls -A ${JENKINS_HOME}/jobs/)" ] ; then
@@ -96,10 +96,6 @@ function main() {
   # mv -f "${TMP_TAR_NAME}" "${DEST_FILE}"
   #
   # cleanup
-
-  ls -R "${ARC_DIR}"
-
-  du -hs "${ARC_DIR}"
 
   exit 0
 }
